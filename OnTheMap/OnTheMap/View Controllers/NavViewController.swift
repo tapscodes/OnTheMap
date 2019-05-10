3eae6eb9f3c6206e3b5281f1722bf320ad9da2c2
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-05-10 11:44:00 -0400

    Added navigation buttons
    I added some buttons for navigating the application that should've been there.

---
+++ OnTheMap/OnTheMap/View Controllers/NavViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NavViewController.swift
+//  OnTheMap
+//
+//  Created by Tristan Pudell-Spatscheck on 5/10/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class NavViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //lougout button pressed
+    @IBAction func lougoutPressed(_ sender: Any) {
+    }
+    //refresh button pressed
+    @IBAction func refreshPressed(_ sender: Any) {
+    }
+    //add location pressed
+    @IBAction func newLocPressed(_ sender: Any) {
+    }
+}
---
+++ OnTheMap/OnTheMap/View Controllers/NewLocViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewLocViewController.swift
+//  OnTheMap
+//
+//  Created by Tristan Pudell-Spatscheck on 5/10/19.
+//  Copyright © 2019 TAPS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class NewLocViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //prssed find location button
+    @IBAction func findLocationPressed(_ sender: Any) {
+    }
+}
