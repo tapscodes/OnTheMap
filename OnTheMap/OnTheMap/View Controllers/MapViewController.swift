5ef4dffacae863ce866be65870fa4a83ded0a5ed
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2019-05-18 23:31:57 -0400

    Finished The Project
    Fixxed all bugs and made the Map load correctly

--- OnTheMap/OnTheMap/View Controllers/MapViewController.swift
+++ OnTheMap/OnTheMap/View Controllers/MapViewController.swift
@@ -5,29 +5,46 @@
 //  Created by Tristan Pudell-Spatscheck on 5/2/19.
 //  Copyright © 2019 TAPS. All rights reserved.
 //
-import UIKit
 import Foundation
-import CoreLocation
+import UIKit
 import MapKit
+
+/**
+ * This view controller demonstrates the objects involved in displaying pins on a map.
+ *
+ * The map is a MKMapView.
+ * The pins are represented by MKPointAnnotation instances.
+ *
+ * The view controller conforms to the MKMapViewDelegate so that it can receive a method
+ * invocation when a pin annotation is tapped. It accomplishes this using two delegate
+ * methods: one to put a small "info" button on the right side of each pin, and one to
+ * respond when the "info" button is tapped.
+ */
+
 class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    // The map. See the setup in the Storyboard file. Note particularly that the view controller
+    // is set up as the map view's delegate.
     @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
-            super.viewDidLoad()
-            // The "locations" array is an array of dictionary objects that are similar to the JSON
-            // data that you can download from parse.
-             studentLoc.getStudentLocation()
+        super.viewDidLoad()
+        
+        studentLoc.getStudentLocation()
         let locations = studentLoc.getStudentResp()?.results
-            
-            // We will create an MKPointAnnotation for each dictionary in "locations". The
-            // point annotations will be stored in this array, and then provided to the map view.
+        
+        
+        // We will create an MKPointAnnotation for each dictionary in "locations". The
+        // point annotations will be stored in this array, and then provided to the map view.
         var annotations = [MKPointAnnotation]()
-            
-            // The "locations" array is loaded with the sample data below. We are using the dictionaries
-            // to create map annotations. This would be more stylish if the dictionaries were being
-            // used to create custom structs. Perhaps StudentLocation structs.
+        
+        // The "locations" array is loaded with the sample data below. We are using the dictionaries
+        // to create map annotations. This would be more stylish if the dictionaries were being
+        // used to create custom structs. Perhaps StudentLocation structs.
+        
         for dictionary in locations! {
-                // Notice that the float values are being used to create CLLocationDegree values.
-                // This is a version of the Double type.
+            // Notice that the float values are being used to create CLLocationDegree values.
+            // This is a version of the Double type.
             if (dictionary.latitude != nil && dictionary.longitude != nil) {
                 let lat = CLLocationDegrees(dictionary.latitude as! Float)
                 let long = CLLocationDegrees(dictionary.longitude as! Float)
@@ -46,15 +63,13 @@ class MapViewController: UIViewController, MKMapViewDelegate {
                 // Finally we place the annotation in an array of annotations.
                 annotations.append(annotation)
             }
-            }
-            
-            // When the array is complete, we add the annotations to the map after removing the old ones.
-            self.mapView.removeAnnotations(self.mapView.annotations)
-            self.mapView.addAnnotations(annotations)
+        }
+        
+        // When the array is complete, we add the annotations to the map after removing the current values
+        self.mapView.removeAnnotations(self.mapView.annotations)
+        self.mapView.addAnnotations(annotations)
+        
     }
-}
-    
-    //MAP FUNCTIONS
     
     // MARK: - MKMapViewDelegate
     
@@ -62,7 +77,6 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
     // method in TableViewDataSource.
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
         let reuseId = "pin"
         
         var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
@@ -70,7 +84,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         if pinView == nil {
             pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
             pinView!.canShowCallout = true
-            pinView!.pinTintColor = .red
+            pinView!.pinColor = .red
             pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
         else {
@@ -87,7 +101,9 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         if control == view.rightCalloutAccessoryView {
             let app = UIApplication.shared
             if let toOpen = view.annotation?.subtitle! {
-                app.open(URL(string: toOpen)!, options: [:], completionHandler: nil)
+                app.open(URL(string: toOpen)!)
             }
         }
     }
+    
+}
